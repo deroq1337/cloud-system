f4d4fac4ec1e554a6057a3fe84a5879d12b33c81
deroq1337 <miles051104@gmail.com>
2025-02-20 23:10:46 +0100

    feat(GameFileManager): added agreeEULA method

--- master/src/main/java/com/github/deroq1337/cloud/master/data/game/GameFileManager.java
+++ master/src/main/java/com/github/deroq1337/cloud/master/data/game/GameFileManager.java
@@ -8,12 +8,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
+import java.util.concurrent.TimeUnit;
 
 public class GameFileManager {
+
+    private static final String SERVER_JAR_FILE_NAME = "server.jar";
+    private static final long SERVER_JAR_EXECUTION_TIMEOUT = 90 * 1000L;
 
     private final @NotNull CloudSystemMaster master;
     private final @NotNull Path gamesDirectory;
@@ -26,18 +27,16 @@ public class GameFileManager {
     public @NotNull ListenableFuture<Boolean> initGameFiles(@NotNull String gameId, @NotNull InputStream serverJarInput) {
         return Futures.submitAsync(() -> {
             try (serverJarInput) {
-                Path gameDirectory = gamesDirectory.resolve(gameId + "/");
+                Path gameDirectory = gamesDirectory.resolve(gameId).resolve("templates");
                 Files.createDirectories(gameDirectory);
 
-                Path serverJarPath = Paths.get(gameDirectory.toString(), "server.jar");
+                Path serverJarPath = gameDirectory.resolve(SERVER_JAR_FILE_NAME);
                 Files.copy(serverJarInput, serverJarPath, StandardCopyOption.REPLACE_EXISTING);
 
-                Path gameServerTemplatesDirectory = gameDirectory.resolve("templates/");
-                Files.createDirectories(gameServerTemplatesDirectory);
-
+                master.getLog().info("Initialized game files for '{}'", gameId);
                 return Futures.immediateFuture(true);
             } catch (IOException e) {
-                master.getLog().error("Error initializing files of Game '{}'", gameId, e);
+                master.getLog().error("Error initializing files of game '{}'", gameId, e);
                 return Futures.immediateFailedFuture(e);
             }
         }, AsyncExecutor.EXECUTOR_SERVICE);
@@ -46,14 +45,10 @@ public class GameFileManager {
     public @NotNull ListenableFuture<Boolean> initTemplateFiles(@NotNull String gameId, @NotNull String templateId) {
         return Futures.submitAsync(() -> {
             try {
-                Path gameDirectory = gamesDirectory.resolve(gameId + "/");
-                Path templatesDirectory = gameDirectory.resolve("templates/");
-                Files.createDirectories(templatesDirectory);
-
-                Path templateDirectory = templatesDirectory.resolve(templateId + "/");
+                Path templateDirectory = getTemplateDirectory(gameId, templateId);
                 Files.createDirectories(templateDirectory);
 
-                Path gameServerJar = gameDirectory.resolve("server.jar");
+                Path gameServerJar = gamesDirectory.resolve(gameId).resolve(SERVER_JAR_FILE_NAME);
                 Path templateServerJar = templateDirectory.resolve(gameServerJar.getFileName().toString());
                 Files.copy(gameServerJar, templateServerJar, StandardCopyOption.REPLACE_EXISTING);
 
@@ -61,12 +56,37 @@ public class GameFileManager {
                 processBuilder.directory(templateDirectory.toFile());
                 Process process = processBuilder.start();
 
-                process.waitFor();
-                process.destroy();
+                boolean finished = process.waitFor(SERVER_JAR_EXECUTION_TIMEOUT, TimeUnit.MILLISECONDS);
+                if (!finished) {
+                    process.destroyForcibly();
+                    throw new IllegalStateException("Server initialization timed out for template: " + templateId);
+                }
 
+                master.getLog().info("Initialized template files for '{}'", templateId);
                 return Futures.immediateFuture(true);
             } catch (Exception e) {
-                master.getLog().error("Error initializing files of Template '{}'", templateId, e);
+                master.getLog().error("Error initializing files of template '{}'", templateId, e);
+                return Futures.immediateFailedFuture(e);
+            }
+        }, AsyncExecutor.EXECUTOR_SERVICE);
+    }
+
+    private @NotNull Path getTemplateDirectory(@NotNull String gameId, @NotNull String templateId) {
+        return gamesDirectory.resolve(gameId)
+                .resolve("templates")
+                .resolve(templateId);
+    }
+
+    public @NotNull ListenableFuture<Boolean> agreeEULA(@NotNull String gameId, @NotNull String templateId) {
+        return Futures.submitAsync(() -> {
+            try {
+                Path eulaFile = getTemplateDirectory(gameId, templateId).resolve("eula.txt");
+                Files.writeString(eulaFile, "eula=true", StandardOpenOption.TRUNCATE_EXISTING);
+
+                master.getLog().info("Agreed to EULA for template '{}'", templateId);
+                return Futures.immediateFuture(true);
+            } catch (Exception e) {
+                master.getLog().error("Error agreeing to EULA for template '{}'", templateId, e);
                 return Futures.immediateFailedFuture(e);
             }
         }, AsyncExecutor.EXECUTOR_SERVICE);
